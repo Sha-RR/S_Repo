ab72a2cab37c8234589d124e3d2b09524dad73eb
Shaikshavali Shaik <shaikshav007@gmail.com>
2026-08-24 16:40:49 -0700

    Update qa-data.js

--- qa-data.js
+++ qa-data.js
@@ -1,6 +1,4 @@
 const qaData = [
-  /*{
-    question: " -->}*/
   {
     question: "SEO problems.",
     answer: `These are the errors or oversights that prevent search engines from crawling, indexing, or understanding your site's
